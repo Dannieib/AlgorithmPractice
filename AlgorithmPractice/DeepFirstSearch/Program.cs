17783f4e417694fb24698057e730f74336a44f07
Daniel Ibanga <danielibanga4321@outlook.com>
2021-08-30 22:15:28 +0100

    parking dilimma

---
+++ AlgorithmPractice/DeepFirstSearch/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace DeepFirstSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello World!");
+        }
+
+
+        public List<string> DepthFirstSearch(List<string> array)
+        {
+            // Write your code here.
+            List<string> output = new List<string>();
+
+            for (int i = 0; i < array.Count; i++)
+            {
+                //output.Add(array[i]);
+                //recursive pattern
+            }
+            return output;
+        }
+    }
+
+
+    public class Node
+    {
+        public string name;
+        public List<Node> children = new List<Node>();
+
+        public Node(string name)
+        {
+            this.name = name;
+        }
+
+
+        public Node AddChild(string name)
+        {
+            Node child = new Node(name);
+            children.Add(child);
+            return this;
+        }
+    }
+}
---
+++ AlgorithmPractice/ParkingDelimma/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ParkingDelimma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] cars = { 2, 10, 08, 17 };
+            Console.WriteLine(Parkingdelimma(cars, 3));
+
+            Console.ReadKey();
+        }
+
+        public static int Parkingdelimma(int[] cars, int k)
+        {
+            Array.Sort(cars);
+
+            int min = int.MaxValue;
+
+            for (int i = 0; i < cars.Length - k + 1; i++)
+            {
+                min = Math.Min(min, cars[i + k - 1] - cars[i]);
+            }
+            return min + 1;
+        }
+    }
+}
